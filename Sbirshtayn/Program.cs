eb88da1d90c121e7d85811eda028d5a371b3e729
spareva <spareva@abv.bg>
2013-11-28 14:48:30 +0200

    Upload
    Project Earthy

---
+++ Sbirshtayn/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sbirshtayn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process cmd = new Process();
+            // initialize some things
+            Init(cmd);
+            
+
+
+            string execDir = AppDomain.CurrentDomain.BaseDirectory;
+            string[] dirs = Directory.GetDirectories(execDir);
+
+            foreach (var dir in dirs)
+            {
+                string[] fileNames = GetFileNames(dir, "*.tif");
+                foreach (var fileName in fileNames)
+                {
+                    TranslateTifftoVrt(dir +"\\"+ fileName, cmd);
+                }
+            }
+
+
+            // finalize
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+
+        private static void Init(Process cmd)
+        {
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+
+            cmd.Start();
+
+            /* execute "dir" */
+            // Set fw tools environment
+            cmd.StandardInput.WriteLine(@"C:\""Program Files (x86)""\FWTools2.4.7\setfw.bat");
+            cmd.StandardInput.WriteLine(@"C:\""Program Files (x86)""\FWTools2.4.7\bin\setfwenv.bat");
+            //cmd.StandardInput.WriteLine(@"gdalinfo c:\users\nikita\desktop\AERONET_BAHRAIN.2013104.terra.250m.tif > c:\users\nikita\desktop\test.txt");
+        }
+
+
+        private static string[] GetFileNames(string path, string filter)
+        {
+            string[] files = Directory.GetFiles(path, filter);
+            for(int i = 0; i < files.Length; i++)
+                files[i] = Path.GetFileName(files[i]);
+            return files;
+        }
+
+        static void TranslateTifftoVrt(string fileName, Process cmd)
+        {
+            cmd.StandardInput.WriteLine("gdal_translate -of VRT " + fileName + " " + fileName + ".vrt");
+        }
+
+        string GenerateCombinedVrt(List<string> filePaths)
+        {
+            return null;
+        }
+
+        void TranslateVrtToTiff(string filePath)
+        {
+
+        }
+
+        void CreatePyramidFile(string filePath)
+        {
+
+        }
+    }
+}
---
+++ converter/VRT Editor/VRTParser.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VRT_Editor
+{
+    class VRTParser
+    {
+        static void Main(string[] args)
+        {
+            List<string> newFile = new List<string>();
+            List<string> outputFile = new List<string>();
+            List<string> rasterBand = new List<string>();
+            List<StringBuilder> newRasterBand = new List<StringBuilder>();
+
+            ReadFile(newFile, @"1.vrt");
+
+            bool isFound = false;
+
+            foreach (var line in newFile)
+            {
+                if (line.Equals("  <VRTRasterBand dataType=\"Byte\" band=\"1\">"))
+                {
+                    isFound = true;
+                }
+                if (isFound)
+                {
+                    rasterBand.Add(line);
+                }
+                else
+                {
+                    outputFile.Add(line);
+                }
+                if (line.Equals("  </VRTRasterBand>"))
+                {
+                    isFound = false;
+                    break;
+                }
+            }
+
+            string vrtRasterMatch = "band=\"";
+            string tifMatch = "0.tif</SourceFilename>";
+
+            for (int band = 1; band <= 7; band++)
+            {
+                if (band == 6)
+                {
+                    continue;
+                }
+                for (int line = 0; line < rasterBand.Count; line++)
+                {
+                    StringBuilder initialize = new StringBuilder();
+                    newRasterBand.Add(initialize);
+                    if (line == 0)
+                    {
+                        int bandIndex = rasterBand[line].IndexOf(vrtRasterMatch);
+                        string vrtRasterBand = rasterBand[line].Substring(0, bandIndex + vrtRasterMatch.Length);
+                        newRasterBand[line].Append(vrtRasterBand);
+                        newRasterBand[line].Append(band.ToString());
+                        newRasterBand[line].Append(rasterBand[line].Substring(bandIndex + vrtRasterMatch.Length + 1));
+                    }
+                    else if (line == 4)
+                    {
+                        int tifIndex = rasterBand[line].IndexOf(tifMatch);
+                        string tifString = rasterBand[line].Substring(tifIndex);
+                        newRasterBand[line].Append(rasterBand[line].Substring(0, tifIndex - 1));
+                        newRasterBand[line].Append(band.ToString());
+                        newRasterBand[line].Append(tifString);          
+                    }
+                    else
+                    {
+                        newRasterBand[line].Append(rasterBand[line]);
+                    }
+                    outputFile.Add(newRasterBand[line].ToString());
+                }
+                newRasterBand.Clear();
+            }
+
+            outputFile.Add("</VRTDataset>");
+
+            WriteFile(outputFile, @"output.txt");
+        }
+
+        private static void WriteFile(List<string> newFile, string fileName)
+        {
+            StreamWriter writer = new StreamWriter(fileName);
+            using (writer)
+            {
+                for (int i = 0; i < newFile.Count; i++)
+                {
+                    writer.WriteLine(newFile[i]);
+                }
+            }
+        }
+
+        private static void ReadFile(List<string> newFile, string fileName)
+        {
+            StreamReader reader = new StreamReader(fileName);
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    newFile.Add(line);
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
